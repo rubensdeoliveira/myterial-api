9b4aacc0e7f64c6601bfff47fcfd9d573bf8403a
rubensojunior <rubensojunior6@gmail.com>
2020-07-15 09:57:56 -0300

    configuring database and creating users table

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express from "express";
+import express from 'express'
+import routes from './routes'
+import './database'
 
-import routes from "./routes";
+const app = express()
 
-const app = express();
-
-app.use(express.json());
-
-app.use(routes);
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
-  console.log("Server stated on port 3333");
-});
+  console.log('Server stated on port 3333')
+})
