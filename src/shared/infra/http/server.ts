3fe5839fd456e741fde200d85b68b9a51d5dd61a
rubensojunior <rubensojunior6@gmail.com>
2020-07-16 14:48:58 -0300

    implementing storage provider

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata'
 
 import express, { Request, Response, NextFunction } from 'express'
+// import cors from 'cors'
 import 'express-async-errors'
-import AppError from '@shared/errors/AppError'
+
 import uploadConfig from '@config/upload'
+import AppError from '@shared/errors/AppError'
 import routes from './routes'
 
 import '@shared/infra/typeorm'
@@ -11,8 +13,9 @@ import '@shared/container'
 
 const app = express()
 
+// app.use(cors())
 app.use(express.json())
-app.use('/files', express.static(uploadConfig.directory))
+app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
