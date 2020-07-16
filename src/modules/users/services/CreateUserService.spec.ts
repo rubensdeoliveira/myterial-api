6083438c484ec4cb7b9f3eb23b244b4ce4a24ac8
rubensojunior <rubensojunior6@gmail.com>
2020-07-16 16:22:08 -0300

    implementing create user test

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,43 @@
-test('sum two number', () => {
-  expect(1 + 2).toBe(3)
+import AppError from '@shared/errors/AppError'
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import CreateUserService from './CreateUserService'
+
+let fakeUsersRepository: FakeUsersRepository
+let fakeHashProvider: FakeHashProvider
+let createUser: CreateUserService
+
+describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository()
+    fakeHashProvider = new FakeHashProvider()
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+  })
+
+  it('should be able to create a new user', async () => {
+    const user = await createUser.execute({
+      name: 'Guilherme Oliveira',
+      email: 'guilhermeoliveira@gmail.com',
+      password: '12345678',
+    })
+
+    expect(user).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new user with same email', async () => {
+    await createUser.execute({
+      name: 'Guilherme Oliveira',
+      email: 'guilhermeoliveira@gmail.com',
+      password: '12345678',
+    })
+
+    await expect(
+      createUser.execute({
+        name: 'Guilherme Oliveira',
+        email: 'guilhermeoliveira@gmail.com',
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
 })
